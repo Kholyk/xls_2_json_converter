48874d7dd57dc7c93ec1453d12f05c96536aeabe
Oleg Kholyk <oleg@kholyk.ru>
2018-12-08 14:45:11 +0300

    now it works

--- Makefile
+++ Makefile
@@ -2,8 +2,6 @@ install:
 	npm install
 start:
 	npm run babel-node -- dist/bin/convert.js
-teststart:
-	npm run babel-node -- src/index.js
 build:
 	npm run build
 publish:
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import readlineSync from 'readline-sync';
-// import xlsToJson from 'xls-to-json';
+import xlsToJson from 'xls-to-json';
 import getArgFromCLI from 'command-line-commands';
 
 /**
@@ -18,36 +18,29 @@ console.log('argv:    %s', JSON.stringify(argv))
 // const outputFileName = readlineSync.question('\r\nInput filename of JSON file: ');
 // console.log(`\r\n Your file has hame: ${fileName}`);
 
-// const catchError = (err, result) => {
-//   if (err) {
-//     console.error(err);
-//   } else {
-//     console.log(result);
-//   }
-// };
+const catchError = (err, result) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(result);
+  }
+};
 
-// const exec = (inputFile, outputFile, datasheet) => {
-//   const args = {
-//     input: inputFile,
-//     output: outputFile,
-//     sheet: datasheet,
-//   };
+const exec = (inputFile, outputFile, datasheet) => {
+  const args = {
+    input: inputFile,
+    output: outputFile,
+    sheet: datasheet,
+  };
 
-//   return xlsToJson(args, catchError);
-// };
-
-// test
-const valid = ['null', 'save'];
-const { cmd, arg } = getArgFromCLI(valid);
-
-console.log('command: %s', cmd);
-console.log('argv:    %s', JSON.stringify(arg));
-// test
+  return xlsToJson(args, catchError);
+};
 
 export default () => {
   const validCommands = ['null', 'save'];
   const { command, argv } = getArgFromCLI(validCommands);
 
-  console.log('command: %s', command);
-  console.log('argv:    %s', JSON.stringify(argv));
+  return (command === 'save' && argv.length !== 0) ?
+    exec(argv[0], argv[1] || `${argv[0]}.json`, argv[2] || null)
+    : -1;
 };
